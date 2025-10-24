1e4b82f7238cb95809d2a9dd1d8b3a1bba94c531
DivyanshTech06 <divyanshtech06@gmail.com>
2025-10-24 20:30:04 +0530

    chore: edit index.ts in express and deleted redundant files

--- packages/dca-backend/src/lib/express/index.ts
+++ packages/dca-backend/src/lib/express/index.ts
@@ -6,39 +6,26 @@ import helmet from 'helmet';
 import { createVincentUserMiddleware } from '@lit-protocol/vincent-app-sdk/expressMiddleware';
 import { getAppInfo, getPKPInfo, isAppUser } from '@lit-protocol/vincent-app-sdk/jwt';
 
-import { handleListPurchasesRoute } from './purchases';
 import { router } from './rules';
-import {
-  handleListSchedulesRoute,
-  handleEnableScheduleRoute,
-  handleDisableScheduleRoute,
-  handleCreateScheduleRoute,
-  handleDeleteScheduleRoute,
-  handleEditScheduleRoute,
-} from './schedules';
 import { userKey, VincentAuthenticatedRequest } from './types';
 import { env } from '../env';
 import { serviceLogger } from '../logger';
 
 const { ALLOWED_AUDIENCE, CORS_ALLOWED_DOMAIN, IS_DEVELOPMENT, VINCENT_APP_ID } = env;
-
 const { handler, middleware } = createVincentUserMiddleware({
   userKey,
   allowedAudience: ALLOWED_AUDIENCE,
   requiredAppId: VINCENT_APP_ID,
 });
-
 const corsConfig = {
   optionsSuccessStatus: 204,
   origin: IS_DEVELOPMENT ? true : [CORS_ALLOWED_DOMAIN],
 };
-
 const setSentryUserMiddleware = handler(
   (req: VincentAuthenticatedRequest, res: Response, next: NextFunction) => {
     if (!isAppUser(req.user.decodedJWT)) {
       throw new Error('Vincent JWT is not an app user');
     }
-
     Sentry.setUser({
       app: getAppInfo(req.user.decodedJWT),
       ethAddress: getPKPInfo(req.user.decodedJWT).ethAddress,
@@ -46,46 +33,15 @@ const setSentryUserMiddleware = handler(
     next();
   }
 );
-
 export const registerRoutes = (app: Express) => {
   app.use(helmet());
   app.use(express.json());
-
   if (IS_DEVELOPMENT) {
     serviceLogger.info(`CORS is disabled for development`);
   } else {
     serviceLogger.info(`Configuring CORS with allowed domain: ${CORS_ALLOWED_DOMAIN}`);
   }
   app.use(cors(corsConfig));
-
-  app.get('/purchases', middleware, setSentryUserMiddleware, handler(handleListPurchasesRoute));
-  app.get('/schedules', middleware, setSentryUserMiddleware, handler(handleListSchedulesRoute));
-  app.post('/schedule', middleware, setSentryUserMiddleware, handler(handleCreateScheduleRoute));
-  app.put(
-    '/schedules/:scheduleId',
-    middleware,
-    setSentryUserMiddleware,
-    handler(handleEditScheduleRoute)
-  );
-  app.put(
-    '/schedules/:scheduleId/enable',
-    middleware,
-    setSentryUserMiddleware,
-    handler(handleEnableScheduleRoute)
-  );
-  app.put(
-    '/schedules/:scheduleId/disable',
-    middleware,
-    setSentryUserMiddleware,
-    handler(handleDisableScheduleRoute)
-  );
-  app.delete(
-    '/schedules/:scheduleId',
-    middleware,
-    setSentryUserMiddleware,
-    handler(handleDeleteScheduleRoute)
-  );
-
   app.use('/api/v1/rules', middleware, setSentryUserMiddleware, handler(router));
   serviceLogger.info(`Routes registered`);
 };
